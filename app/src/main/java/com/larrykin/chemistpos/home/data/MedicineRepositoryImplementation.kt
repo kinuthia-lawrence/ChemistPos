501e5dd173a02023518bd6367562ace1f00a6a5e
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-10-12 10:53:26 +0300

    modified the  medicine and supplier crud

--- app/src/main/java/com/larrykin/chemistpos/home/data/MedicineRepositoryImplementation.kt
+++ app/src/main/java/com/larrykin/chemistpos/home/data/MedicineRepositoryImplementation.kt
@@ -3,31 +3,71 @@ package com.larrykin.chemistpos.home.data
 import com.larrykin.chemistpos.core.data.GetAllMedicinesResult
 import com.larrykin.chemistpos.home.domain.MedicineRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MedicineRepositoryImplementation @Inject constructor(private val medicineDao: MedicineDao) :
     MedicineRepository {
+        //insert medicine
     override suspend fun insertMedicine(medicine: Medicine): Long? {
-        TODO("Not yet implemented")
+        return try {
+            medicineDao.insert(medicine)
+        } catch (e: Exception) {
+            -1
+        }
     }
 
+    //get all medicines
     override suspend fun getMedicines(): Flow<GetAllMedicinesResult> {
-        TODO("Not yet implemented")
+        return try {
+            medicineDao.getAllMedicines().map { medicines ->
+                if (medicines.isEmpty()) {
+                    GetAllMedicinesResult.Success(emptyList())
+                } else {
+                    GetAllMedicinesResult.Success(medicines)
+                }
+            }
+        } catch (e: Exception) {
+            flowOf(GetAllMedicinesResult.Error(e.message ?: "An error occurred"))
+        }
     }
 
+    //get medicine by id
     override suspend fun getMedicineById(medicineId: Int): Medicine? {
-        TODO("Not yet implemented")
+        return try {
+            medicineDao.getMedicineById(medicineId).firstOrNull()
+        }catch (e: Exception){
+            null
+        }
     }
 
+    //get medicine by name
     override suspend fun getMedicineByName(medicineName: String): Medicine? {
-        TODO("Not yet implemented")
+        return try {
+            medicineDao.getMedicineByName(medicineName).firstOrNull()
+        }catch (e: Exception){
+            null
+        }
     }
 
+    //update medicine
     override suspend fun updateMedicine(medicine: Medicine): Medicine? {
-        TODO("Not yet implemented")
+        return try {
+            val rowsUpdated = medicineDao.updateMedicine(medicine)
+            if (rowsUpdated > 0) medicineDao.getMedicineById(medicine.id).firstOrNull() else null
+        }catch (e: Exception){
+            null
+        }
     }
 
+    //delete medicine
     override suspend fun deleteMedicine(medicineId: Int): Boolean {
-        TODO("Not yet implemented")
+        return try{
+            medicineDao.deleteMedicine(medicineId) > 0
+        }catch (e: Exception){
+            false
+        }
     }
 }
--- app/src/main/java/com/larrykin/chemistpos/home/data/SupplierRepositoryImplementation.kt
+++ app/src/main/java/com/larrykin/chemistpos/home/data/SupplierRepositoryImplementation.kt
@@ -3,31 +3,71 @@ package com.larrykin.chemistpos.home.data
 import com.larrykin.chemistpos.core.data.GetAllSuppliersResult
 import com.larrykin.chemistpos.home.domain.SupplierRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class SupplierRepositoryImplementation @Inject constructor(private val supplierDao : SupplierDao) :
     SupplierRepository {
+        //insert supplier
     override suspend fun insertSupplier(supplier: Supplier): Long? {
-        TODO("Not yet implemented")
+        return try{
+            supplierDao.insert(supplier)
+        }catch (e: Exception){
+            -1
+        }
     }
 
+    //get all suppliers
     override suspend fun getAllSuppliers(): Flow<GetAllSuppliersResult> {
-        TODO("Not yet implemented")
+        return try{
+            supplierDao.getAllSuppliers().map { suppliers ->
+                if (suppliers.isEmpty()){
+                    GetAllSuppliersResult.Success(emptyList())
+                }else{
+                    GetAllSuppliersResult.Success(suppliers)
+                }
+            }
+        }catch (e: Exception){
+            flowOf(GetAllSuppliersResult.Error(e.message ?: "An error occurred"))
+        }
     }
 
+    //get supplier by id
     override suspend fun getSupplierById(supplierId: Int): Supplier? {
-        TODO("Not yet implemented")
+        return try {
+            supplierDao.getSupplierById(supplierId).firstOrNull()
+        }catch (e: Exception){
+            null
+        }
     }
 
+    //get supplier by name
     override suspend fun getSupplierByName(supplierName: String): Supplier? {
-        TODO("Not yet implemented")
+        return try {
+            supplierDao.getSupplierByName(supplierName).firstOrNull()
+        }catch (e: Exception){
+            null
+        }
     }
 
+    //update supplier
     override suspend fun updateSupplier(supplier: Supplier): Supplier? {
-        TODO("Not yet implemented")
+        return try {
+            val rowsUpdated = supplierDao.updateSupplier(supplier)
+            if (rowsUpdated > 0) supplierDao.getSupplierById(supplier.id).firstOrNull() else null
+        }catch (e: Exception){
+            null
+        }
     }
 
+    //delete supplier
     override suspend fun deleteSupplier(supplierId: Int): Boolean {
-        TODO("Not yet implemented")
+        return try {
+            supplierDao.deleteSupplier(supplierId) > 0
+        }catch (e: Exception){
+            false
+        }
     }
 }
