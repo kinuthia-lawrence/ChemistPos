35f5827853f635835007cb18cce4c9a35a3e7185
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-10-24 23:14:09 +0300

    modified the service screen

--- app/src/main/java/com/larrykin/chemistpos/home/presentation/ui/ServicesScreen.kt
+++ app/src/main/java/com/larrykin/chemistpos/home/presentation/ui/ServicesScreen.kt
@@ -1,9 +1,7 @@
 package com.larrykin.chemistpos.home.presentation.ui
 
-import android.app.DatePickerDialog
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Delete
@@ -25,18 +22,16 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -44,7 +39,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -55,14 +49,10 @@ import com.larrykin.chemistpos.components.HeaderText
 import com.larrykin.chemistpos.core.data.LoggedInUser
 import com.larrykin.chemistpos.core.presentation.ui.CustomAlertDialog
 import com.larrykin.chemistpos.core.presentation.ui.CustomAlertDialogWithChoice
-import com.larrykin.chemistpos.home.data.Product
-import com.larrykin.chemistpos.home.data.Service
 import com.larrykin.chemistpos.home.data.ServicesOffered
 import com.larrykin.chemistpos.home.presentation.viewModels.ServiceResult
 import com.larrykin.chemistpos.home.presentation.viewModels.ServicesViewModel
-import com.larrykin.chemistpos.home.presentation.viewModels.StockResult
 import kotlinx.coroutines.launch
-import java.util.Calendar
 import java.util.Date
 
 @Composable
@@ -70,7 +60,7 @@ fun ServicesScreen(
     loggedInUser: LoggedInUser,
     servicesViewModel: ServicesViewModel = hiltViewModel()
 ) {
-    var selectedTabIndex by remember { mutableStateOf(0) }
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
     val tabs = listOf("Add Service", "Service History")
 
     Column(modifier = Modifier.fillMaxSize()) {
@@ -111,10 +101,14 @@ fun ServicesHistory(loggedInUser: LoggedInUser, servicesViewModel: ServicesViewM
                     errorMessage = result.message
                 }
 
-                ServiceResult.ServiceExists -> TODO()
-                ServiceResult.ServiceNotFound -> TODO()
+                ServiceResult.ServiceExists ->  {
+                    errorMessage = "Service already exists"
+                }
+                ServiceResult.ServiceNotFound ->  {
+                    errorMessage = "Service not found"
+                }
             }
-        } as (List<ServicesOffered>) -> Unit
+        }
     }
 
     Column(modifier = Modifier.padding(16.dp)) {
@@ -160,7 +154,7 @@ fun ServicesHistory(loggedInUser: LoggedInUser, servicesViewModel: ServicesViewM
 
 @Composable
 fun AddService(loggedInUser: LoggedInUser, servicesViewModel: ServicesViewModel) {
-    TODO("Not yet implemented")
+    Text(text = "add service")
 }
 
 @Composable
@@ -209,7 +203,7 @@ fun ServiceCard(
 
         // Delete success dialog
         if (showDeleteSuccessDialog) {
-            Log.d("ProductCard", "Service deleted successfully")
+            Log.d("ServiceCard", "Service deleted successfully")
             CustomAlertDialog(
                 title = "Success",
                 message = "Service deleted successfully",
